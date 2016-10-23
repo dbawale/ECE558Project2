f5acbdde050ba5ade13d87bb1b592052be4071d4
Deven Bawale <dbawale@pdx.edu>
2016-10-23 13:41:53 -0700

    Added landscape layout
    Added a layout file for landscape mode
    Handled device rotation using onSaveInstanceState

--- QuizApp/app/src/main/java/project2/dbawale/pdx/edu/quizapp/MainQuizActivity.java
+++ QuizApp/app/src/main/java/project2/dbawale/pdx/edu/quizapp/MainQuizActivity.java
@@ -1,29 +1,45 @@
 package project2.dbawale.pdx.edu.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import project2.dbawale.pdx.edu.quizlibrary.*;
 
 
-public class MainQuizActivity extends AppCompatActivity {
-
+public class MainQuizActivity extends Activity {
+    private final String QUESTION_STRING = "edu.pdx.dbawale.project2.questionstring";
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_quiz);
         final Quiz quiz = new Quiz();
+        final LinearLayout layout = (LinearLayout) findViewById(R.id.activity_main_quiz);
         quiz.setupDefaultQuiz();
         Button button = (Button)findViewById(R.id.button1);
-        final TextView textView = (TextView) findViewById(R.id.textview1);
+        textView = (TextView) findViewById(R.id.textview1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 textView.setText(quiz.getQuestions().get(0).toString());
             }
         });
+        handleInstanceState(savedInstanceState);
+    }
+
+    private void handleInstanceState(Bundle savedInstanceState) {
+        if(savedInstanceState!=null){
+            textView.setText(savedInstanceState.getString(QUESTION_STRING));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle instanceState){
+        instanceState.putString(QUESTION_STRING,textView.getText().toString());
     }
 }

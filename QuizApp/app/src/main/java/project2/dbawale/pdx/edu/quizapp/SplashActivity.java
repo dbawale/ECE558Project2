1dbba481422ef7d4d3f9a7eca432c8fe18040b02
Deven Bawale <dbawale@pdx.edu>
2016-11-05 18:07:56 -0700

    Added a splash screen

---
+++ QuizApp/app/src/main/java/project2/dbawale/pdx/edu/quizapp/SplashActivity.java
@@ -0,0 +1,16 @@
+package project2.dbawale.pdx.edu.quizapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainQuizActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
